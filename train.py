8e7d432ce89d06dd3bf6d20974579b7dfcca5901
Raleigh Slack <raleighslack@gmail.com>
2024-01-07 20:19:38 -0500

    Base model

---
+++ train.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+from roboflow import Roboflow
+import os
+
+model = YOLO('yolov8n.pt')
+
+rf = Roboflow(api_key="fi7NJBusg0KIhDQaFFlK")
+project = rf.workspace("raleigh-slack-idx6f").project("2024-frc")
+dataset = project.version(2).download("yolov8")
+
+model.train(data=f"{dataset.location}/data.yaml", epochs=8, imgsz=640)
+model.val()
