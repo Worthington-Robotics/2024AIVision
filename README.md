84c0c4b1efc3bfcb2e41d0f065dc4c72b96db683
Raleigh Slack <raleighslack@gmail.com>
2024-01-14 15:17:29 -0500

    Update

---
+++ README.md
@@ -0,0 +1,9 @@
+# 2024 AI Vision  
+<a href="https://universe.roboflow.com/raleigh-slack-idx6f/2024-frc/model/">
+    <img src="https://app.roboflow.com/images/try-model-badge.svg"></img>
+</a>
+<a href="https://universe.roboflow.com/raleigh-slack-idx6f/2024-frc">
+    <img src="https://app.roboflow.com/images/download-dataset-badge.svg"></img>
+</a>  
+
+AI Vision for the 2024 game Crescendo by the WorBots 4145.
--- train/train.py
+++ train/train.py
@@ -6,7 +6,8 @@ model = YOLO('yolov8n.pt')
 
 rf = Roboflow(api_key="fi7NJBusg0KIhDQaFFlK")
 project = rf.workspace("raleigh-slack-idx6f").project("2024-frc")
-dataset = project.version(2).download("yolov8")
+dataset = project.version(4).download("yolov8")
 
-model.train(data=f"{dataset.location}/data.yaml", epochs=8, imgsz=640)
+model.train(data=f"{dataset.location}/data.yaml", epochs=1, imgsz=640)
 model.val()
+model.export(format='onnx')
