1e689dd03138278a5acd50703e0b20658e0509e9
Raleigh Slack <raleighslack@gmail.com>
2024-01-14 18:31:26 -0500

    Testing

--- main.py
+++ main.py
@@ -1,5 +1,87 @@
 import cv2
 import argparse
+import numpy as np
+from ultralytics.utils import ASSETS, yaml_load
+from ultralytics.utils.checks import check_yaml
 
-model: cv2.dnn.Net = cv2.dnn.readNetFromONNX('./models/FRC2024.onnx')
-outputs = model.forward()
+CLASSES = yaml_load(check_yaml("./models/data.yaml"))["names"]
+colors = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+def main(onnx_model, input_image):
+    # Load the ONNX Model
+    model: cv2.dnn.Net = cv2.dnn.readNetFromONNX(onnx_model)
+
+    # Read the input image
+    original_image: np.ndarray = cv2.imread(input_image)
+    [height, width, _] = original_image.shape
+
+    # Prepare a square image for inference
+    length = max((height, width))
+    image = np.zeros((length, length, 3), np.uint8)
+    image[0:height, 0:width] = original_image
+
+    # Calculate scale factor
+    scale = length / 640
+
+    # Preprocess the image and prepare blob for model
+    blob = cv2.dnn.blobFromImage(image, scalefactor=1/255, size=(640,640), swapRB=True)
+    model.setInput(blob)
+
+    # Perform inference
+    outputs = model.forward()
+
+    # Prepare output array
+    outputs = np.array([cv2.transpose(outputs[0])])
+    rows = outputs.shape[1]
+
+    boxes = []
+    scores = []
+    class_ids = []
+
+    # Iterate through output to collect bounding boxes, confidence scores, and class IDs
+    for i in range(rows):
+        classes_scores = outputs[0][i][4:]
+        (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
+        if maxScore >= 0.25:
+            box = [
+                outputs[0][i][0] - (0.5 * outputs[0][i][2]),
+                outputs[0][i][1] - (0.5 * outputs[0][i][3]),
+                outputs[0][i][2],
+                outputs[0][i][3],
+            ]
+            boxes.append(box)
+            scores.append(maxScore)
+            class_ids.append(maxClassIndex)
+
+    # Apply Non-Maximum suppression
+    result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.25, 0.45, 0.5)
+    detections = []
+    for i in range(len(result_boxes)):
+        index = result_boxes[i]
+        box = boxes[index]
+        detection = {
+            "class_id": class_ids[index],
+            "class_name": CLASSES[class_ids[index]],
+            "confidence": scores[index],
+            "box": box,
+            "scale": scale
+        }
+        detections.append(detection)
+        drawBoundingBox(original_image, class_ids[index], scores[index], round(box[0] * scale), round(box[1] * scale), round((box[0] + box[2]) * scale), round((box[1] + box[3]) * scale))
+    cv2.imwrite("image.jpg", original_image)
+
+    return detections
+
+def drawBoundingBox(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+    label = f"{CLASSES[class_id]} ({confidence:.2f})"
+    color = colors[class_id]
+    cv2.rectangle(img, (x,y), (x_plus_w, y_plus_h), color, 2)
+    cv2.putText(img, label, (x-10,y-10), cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 2)
+    #'./models/FRC2024.onnx'
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", default="yolov8n.onnx", help="Inputs the filename of your ONNX model.")
+    parser.add_argument("--img", default="bus.jpg", help="Path to image")
+    args = parser.parse_args()
+    main(args.model, args.img)
