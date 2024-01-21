bad2a04494fd48f4b1f8e4135e9f878940f771cc
Raleigh Slack <raleighslack@gmail.com>
2024-01-21 11:04:55 -0500

    Update train.py

--- train/train.py
+++ train/train.py
@@ -2,13 +2,12 @@ from ultralytics import YOLO
 from roboflow import Roboflow
 import os
 
-model = YOLO('yolov8n.pt')
+model = YOLO('yolov8s.pt')
 
 rf = Roboflow(api_key="fi7NJBusg0KIhDQaFFlK")
 project = rf.workspace("raleigh-slack-idx6f").project("2024-frc")
-dataset = project.version(5).download("yolov8")
+dataset = project.version(6).download("yolov8")
 
 model.train(data=f"{dataset.location}/data.yaml", epochs=500, imgsz=640, patience=500, plots=True)
 model.val()
 model.export(format='onnx')
-model.export(format='tflite')
