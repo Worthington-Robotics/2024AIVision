c9976fba30098105607e8f23bdc4c2a991b7aa65
Raleigh Slack <raleighslack@gmail.com>
2024-01-24 12:01:46 -0500

    FIx

--- train/train.py
+++ train/train.py
@@ -1,13 +1,21 @@
 from ultralytics import YOLO
 from roboflow import Roboflow
+import argparse
 import os
 
-model = YOLO('yolov8s.pt')
+def main(key):
+    model = YOLO('yolov8n.pt')
 
-rf = Roboflow(api_key="fi7NJBusg0KIhDQaFFlK")
-project = rf.workspace("raleigh-slack-idx6f").project("2024-frc")
-dataset = project.version(6).download("yolov8")
+    rf = Roboflow(api_key=key)
+    project = rf.workspace("worbots-4145").project("2024-frc")
+    dataset = project.version(7).download("yolov8")
 
-model.train(data=f"{dataset.location}/data.yaml", epochs=500, imgsz=640, patience=500, plots=True)
-model.val()
-model.export(format='onnx')
+    model.train(data=f"{dataset.location}/data.yaml", epochs=1000, imgsz=640, patience=1000, plots=True)
+    model.val()
+    model.export(format='onnx')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--key", default="", help="Your roboflow API key")
+    args = parser.parse_args()
+    main(args.key)
